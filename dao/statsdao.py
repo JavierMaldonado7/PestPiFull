1d97a475cf31af9b9cd568b91c22f5b57860c3c9
JaviCuebas <javier.cuebas1@upr.edu>
2024-04-15 21:24:41 -0400

    created stats handler for statsdao

--- dao/statsdao.py
+++ dao/statsdao.py
@@ -11,50 +11,50 @@ class statsDAO:
             pg_config['host'])
         self.conn = psycopg2.connect(connection_url)
 
-def getLastAlert(self):
-    cursor = self.conn.cursor()
-    result = []
-    query = 'nothin yet'
-    cursor.execute(query)
-    for row in cursor:
-        #print("row: ", row)
-        result.append(row)
-    return result
-# SELECT *
-# FROM alerts
-# ORDER BY time DESC
-# LIMIT 5;
+    def getLastAlert(self):
+        cursor = self.conn.cursor()
+        result = []
+        query = 'nothin yet'
+        cursor.execute(query)
+        for row in cursor:
+            #print("row: ", row)
+            result.append(row)
+        return result
+    # SELECT *
+    # FROM alerts
+    # ORDER BY time DESC
+    # LIMIT 5;
 
-def getTop3(self,species):
-    cursor = self.conn.cursor()
-    result = []
-    query = 'nothin yet'
-    cursor.execute(query,(species,))
-    for row in cursor:
-        #print("row: ", row)
-        result.append(row)
-    return result
-# SELECT species, location, COUNT(*) AS alert_count
-#  FROM alerts
-#  WHERE species = %s
-#  GROUP BY location
-#  ORDER BY alert_count DESC
-#  LIMIT 3);
+    def getTop3(self,species):
+        cursor = self.conn.cursor()
+        result = []
+        query = 'nothin yet'
+        cursor.execute(query,(species,))
+        for row in cursor:
+            #print("row: ", row)
+            result.append(row)
+        return result
+    # SELECT species, location, COUNT(*) AS alert_count
+    #  FROM alerts
+    #  WHERE species = %s
+    #  GROUP BY location
+    #  ORDER BY alert_count DESC
+    #  LIMIT 3);
 
-def getSightings(self, species, location, time):
-    cursor = self.conn.cursor()
-    result = []
-    query = 'nothin yet'
-    cursor.execute(query,(species,location, time,))
-    for row in cursor:
-        #print("row: ", row)
-        result.append(row)
-    return result
-# SELECT species, location, COUNT(*) AS sightings_count, MAX(time) AS last_sighting
-# FROM alerts
-# WHERE species = '%s'
-#   AND location = '%s'
-#   AND time >= NOW() - INTERVAL '%s'
-# GROUP BY species, location
-# ORDER BY sightings_count DESC
-# LIMIT 5;
+    def getSightings(self, species, location, time):
+        cursor = self.conn.cursor()
+        result = []
+        query = 'nothin yet'
+        cursor.execute(query,(species,location, time,))
+        for row in cursor:
+            #print("row: ", row)
+            result.append(row)
+        return result
+    # SELECT species, location, COUNT(*) AS sightings_count, MAX(time) AS last_sighting
+    # FROM alerts
+    # WHERE species = '%s'
+    #   AND location = '%s'
+    #   AND time >= NOW() - INTERVAL '%s'
+    # GROUP BY species, location
+    # ORDER BY sightings_count DESC
+    # LIMIT 5;
