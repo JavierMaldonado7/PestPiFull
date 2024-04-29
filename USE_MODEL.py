21990fbff4c31af13d3702567a29adc744755e72
JavierMaldonado7 <37214559+JavierMaldonado7@users.noreply.github.com>
2024-04-29 18:36:52 -0400

    Add files via upload

--- USE_MODEL.py
+++ USE_MODEL.py
@@ -1,41 +1,34 @@
 import os
-
-from ultralytics import YOLO
 import cv2
+from ultralytics import YOLO
 
-
-VIDEOS_DIR = os.path.join('.', 'video')
-
-video_path = os.path.join(VIDEOS_DIR, 'Iguana_2.mp4')
-video_path_out = '{}_out.mp4'.format(video_path)
+video_path = 'test\\RAT.mp4'
+video_path_out = os.path.splitext(video_path)[0] + '_out.mp4'
 
 cap = cv2.VideoCapture(video_path)
+if not cap.isOpened():
+    raise IOError("Cannot open video")
+
 ret, frame = cap.read()
+if not ret:
+    raise ValueError("Can't read the first frame")
+
 H, W, _ = frame.shape
-#out = cv2.VideoWriter(video_path_out, cv2.VideoWriter_fourcc(*'MP4'), int(cap.get(cv2.CAP_PROP_FPS)), (W, H))
-#out = cv2.VideoWriter(video_path_out, cv2.VideoWriter_fourcc(*'H264'), int(cap.get(cv2.CAP_PROP_FPS)), (W, H))
 out = cv2.VideoWriter(video_path_out, cv2.VideoWriter_fourcc(*'mp4v'), int(cap.get(cv2.CAP_PROP_FPS)), (W, H))
 
-
-model_path = 'C:/Users/17872/PycharmProjects/cv-attempt/runs/detect/train8/weights/best.pt'
-
-# Load a model
-model = YOLO(model_path)  # load a custom model
-
-threshold = 0.2
-
+model_path = 'runs\\detect\\train9\\weights\\best.pt'
+model = YOLO(model_path)  # Load a custom model
+threshold = 0.6
 
 while ret:
-
     results = model(frame)[0]
 
     for result in results.boxes.data.tolist():
         x1, y1, x2, y2, score, class_id = result
-        print(results)
         if score > threshold:
             cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 4)
             cv2.putText(frame, results.names[int(class_id)].upper(), (int(x1), int(y1 - 10)),
-                        cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3, cv2.LINE_AA)
+                        cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 255), 3, cv2.LINE_AA)
 
     out.write(frame)
     ret, frame = cap.read()
