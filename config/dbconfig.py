ae52caa91f5920c4fd8143ee2719ed7e71f02879
JaviCuebas <javier.cuebas1@upr.edu>
2024-04-29 15:19:49 -0400

    updated heroku credentials

--- config/dbconfig.py
+++ config/dbconfig.py
@@ -1,8 +1,8 @@
 pg_config = {
 
-    'host': 'ec2-52-5-167-89.compute-1.amazonaws.com',
-    'user': 'uinvwsoqhrrdir',
-    'password': '61030dacbc95ed5d0e86a87fda8166b1b58bf787e14170dab2dc52df3a1f84d0',
-    'dbname': 'd192656rsmtm0u',
+    'host': 'ec2-52-73-67-148.compute-1.amazonaws.com',
+    'user': 'vevnxnnnehlhcr',
+    'password': 'bc565cc08ffecbeeac4ebda9e3362a43eb6b28031322c93304b87bb71a4314d0',
+    'dbname': 'd12fhtfr8lc1ks',
     'dbport': 5432,
 }
